744c554ffc932d565d98d2348ae633c63a69c0f7
s4nderx <phellipe.sander1@gmail.com>
2019-10-27 20:07:23 -0300

    Enviar dados com Jquery + AJAX

--- public/js/frase.js
+++ public/js/frase.js
@@ -1,4 +1,5 @@
 $("#btn-frase").click(fraseAleatoria);
+$("#btn-frase-id").click(buscaFrase);
 
 function fraseAleatoria() {
     $("#spinner").toggle();
@@ -21,5 +22,32 @@ function trocaFraseAleatoria(data) {
     frase.text(data[numeroAleatorio].texto);
     atualizaTamanhoFrase();
     atualizaTempoInicial(data[numeroAleatorio].tempo);
+    $(".frase").toggle();
+}
+
+function buscaFrase(){
+    $("#spinner").toggle();
+    $(".frase").toggle();
+    var fraseId = $("#frase-id").val();
+    console.log("id da frase: " + fraseId)
+
+    var dados = { id: fraseId };
+    
+    $.get("http://localhost:3000/frases", dados, trocaFrase)
+    .fail(function(){
+        $("#erro").show();
+        setTimeout(function(){
+            $("#erro").toggle();
+        }, 3000)
+    }).always(function(){
+        $("#spinner").toggle();
+    });
+}
+
+function trocaFrase(data) {
+    var frase = $(".frase");
+    frase.text(data.texto);
+    atualizaTamanhoFrase();
+    atualizaTempoInicial(data.tempo);
     $(".frase").toggle();
 }
